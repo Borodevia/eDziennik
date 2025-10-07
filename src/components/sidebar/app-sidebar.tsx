89551b2b570087d42cec7e0669011f36c39429ad
Krzysztof Jurkowski <krzysztof.jurkowski@tm1.edu.pl>
2025-10-07 13:03:34 +0000

    Refactor sidebar components and update theme provider structure
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { School } from 'lucide-react';
+import * as React from 'react';
+
+import { NavMail } from '@/components/sidebar/nav-mail';
+import { NavMain } from '@/components/sidebar/nav-main';
+import { NavUser } from '@/components/sidebar/nav-user';
+import { StudentSwitcher } from '@/components/sidebar/student-switcher';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from '@/components/ui/sidebar';
+
+// This is sample data.
+const data = {
+  user: {
+    name: 'shadcn',
+    email: 'm@example.com',
+    avatar: '/avatars/shadcn.jpg',
+  },
+  students: [
+    {
+      school: 'Technikum Mechatroniczne nr 1 im. Piotra Drzewieckiego',
+      symbol: 'TM1',
+      logo: School,
+      student: 'Krzysztof Jurkowski',
+      id: 1,
+    },
+    {
+      school: 'Technikum Mechatroniczne nr 1 im. Piotra Drzewieckiego',
+      symbol: 'TM1',
+      logo: School,
+      student: 'Krzysztof Jurkowski',
+      id: 2,
+    },
+    {
+      school: 'Technikum Mechatroniczne nr 1 im. Piotra Drzewieckiego',
+      symbol: 'TM1',
+      logo: School,
+      student: 'Krzysztof Jurkowski',
+      id: 3,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <StudentSwitcher students={data.students} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain />
+        <NavMail />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
